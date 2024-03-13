a35ad3efff340e7e56f77110539f5564252364a6
kalyanmaram <saikalyan.maram@unh.edu>
2024-03-13 00:57:04 -0400

    pass 6th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,41 +9,50 @@ public class Main {
   public static void main(String[] args) {
     try (ServerSocket serverSocket = new ServerSocket(4221)) {
       serverSocket.setReuseAddress(true);
-      
+
       while (true) {
-        try (Socket clientSocket = serverSocket.accept()) {
-          BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-          OutputStream outputStream = clientSocket.getOutputStream();
-
-          String requestLine = reader.readLine();
-          String[] requestParts = requestLine.split(" ");
-          String path = requestParts[1];
-
-          String userAgent = "";
-          String line;
-          while(!(line= reader.readLine()).equals("")){
-            if(line.startsWith("User-Agent: "))
-            userAgent = line.substring(12);
+        Socket clientSocket = serverSocket.accept();
+        new Thread(()->{
+          try(BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+          OutputStream outputStream = clientSocket.getOutputStream()){
+            String requestLine = reader.readLine();
+            String[] requestParts = requestLine.split(" ");
+            String path = requestParts[1];
+  
+            String userAgent = "";
+            String line;
+            while(!(line= reader.readLine()).equals("")){
+              if(line.startsWith("User-Agent: "))
+              userAgent = line.substring(12);
+            }
+  
+            if(path.startsWith("/user-agent")){
+              String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.length()+ "\r\n\r\n" + userAgent;
+              outputStream.write(response.getBytes());
+            }else if (path.startsWith("/echo/")) {
+              String randomString = path.substring(6);
+              String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + randomString.length()+ "\r\n\r\n" + randomString;
+              outputStream.write(response.getBytes());
+            } else if (path.equals("/")){
+              outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            }else{
+              outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+            }
+            outputStream.flush(); 
+          } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+          } finally{
+            try{
+              clientSocket.close();
+            } catch(IOException e){
+              System.out.println("IOException: " + e.getMessage());
+            }
           }
-
-          if(path.startsWith("/user-agent")){
-            String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.length()+ "\r\n\r\n" + userAgent;
-            outputStream.write(response.getBytes());
-          }else if (path.startsWith("/echo/")) {
-            String randomString = path.substring(6);
-            String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + randomString.length()+ "\r\n\r\n" + randomString;
-            outputStream.write(response.getBytes());
-          } else if (path.equals("/")){
-            outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-          }else{
-            outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-          }
-          outputStream.flush(); 
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        }
+  
+          
+        }).start();
       }
-    } catch (IOException e) {
+      }catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
   }
