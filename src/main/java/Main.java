dcbcd8c2c56f997aee7d168611cab821f33cb8a5
kalyanmaram <saikalyan.maram@unh.edu>
2024-03-12 21:04:02 -0400

    pass 3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,60 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {   
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
+  public static void main(String[] args) {
+    ServerSocket serverSocket = null; // Create a server socket to listen for client connection.
+    Socket clientSocket = null; // Create a socket to communicate with the client.
     try {
       serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
+      System.out.println("Server is listening on port 4221");
 
-      OutputStream outputStream = clientSocket.getOutputStream();
-      outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-      outputStream.flush();
-      clientSocket.close();
-      System.out.println("accepted new connection");
+      while (true) {
+        try (Socket clienSocket = serverSocket.accept()) {
+          BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+          OutputStream outputStream = clientSocket.getOutputStream();
+
+          String requestLine = reader.readLine();
+          String[] requestParts = requestLine.split(" ");
+          String path = requestParts[1];
+
+          if ("/".equals(path)) {
+            outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+          } else {
+            outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+          }
+          outputStream.flush();
+        } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+        }
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
   }
+
 }
+
+// try {
+// serverSocket = new ServerSocket(4221);
+// serverSocket.setReuseAddress(true);
+// clientSocket = serverSocket.accept(); // Accept the client connection.
+// InputStream inputStream = clientSocket.getInputStream();
+
+// // what is InputStream and OutputStream?
+// // InputStream is used to read data from a source and OutputStream is used
+// for writing data to a destination.
+// OutputStream outputStream = clientSocket.getOutputStream(); // Get the output
+// stream to send data to client.
+// outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes()); // Write the HTTP
+// response to the client.
+// outputStream.flush(); // Flush the output stream to send the data to the
+// client.
+// clientSocket.close();
+// System.out.println("accepted new connection");
