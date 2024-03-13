295d6c129f22b947760501859c705930e4301369
kalyanmaram <saikalyan.maram@unh.edu>
2024-03-13 00:39:07 -0400

    pass 5th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,17 @@ public class Main {
           String[] requestParts = requestLine.split(" ");
           String path = requestParts[1];
 
-          if (path.startsWith("/echo/")) {
+          String userAgent = "";
+          String line;
+          while(!(line= reader.readLine()).equals("")){
+            if(line.startsWith("User-Agent: "))
+            userAgent = line.substring(12);
+          }
+
+          if(path.startsWith("/user-agent")){
+            String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.length()+ "\r\n\r\n" + userAgent;
+            outputStream.write(response.getBytes());
+          }else if (path.startsWith("/echo/")) {
             String randomString = path.substring(6);
             String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + randomString.length()+ "\r\n\r\n" + randomString;
             outputStream.write(response.getBytes());
